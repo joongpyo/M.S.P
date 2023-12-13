dfe1489fe1c5e24decc9fa0d8174057ae70f2769
joongpyo <tlswndvy@gmail.com>
2023-12-13 16:45:52 +0900

    Academy Mac 23-12-13/16:45

--- md_exam/src/main/resources/static/sql/medicineuser.sql
+++ md_exam/src/main/resources/static/sql/medicineuser.sql
@@ -9,6 +9,25 @@ user_email varchar(50) not null unique,
 primary key(medicine_id)
 );
 
+create table medicine(
+med_num int not null auto_increment,
+med_name varchar(50) not null,
+med_pregnant char(1) default 'N' not null,
+med_img varchar(100),
+med_efficacy text not null,
+med_type varchar(20) not null,
+med_store char(1) not null,
+med_age int not null,
+primary key(med_num)
+);
 
+create table disease(
+dis_id int not null auto_increment,
+dis_name varchar(20) not null,
+dis_pain_1 varchar(20) not null,
+dis_pain_2 varchar(20),
+dis_pain_3 varchar(20),
+primary key(dis_id)
+);
 
 
