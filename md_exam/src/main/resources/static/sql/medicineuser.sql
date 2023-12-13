5b3aad4bb833a3eb6de58a10141566a8dbfed1f2
joongpyo <tlswndvy@gmail.com>
2023-12-13 12:45:57 +0900

    academy 23-12-13/12:45

--- md_exam/src/main/resources/static/sql/medicineuser.sql
+++ md_exam/src/main/resources/static/sql/medicineuser.sql
@@ -5,7 +5,7 @@ medicine_id int not null auto_increment,
 user_id varchar(20) not null unique,
 user_passwd varchar(20) not null,
 user_name varchar(10) not null,
-user_email varchar(20) not null unique,
+user_email varchar(50) not null unique,
 primary key(medicine_id)
 );
 
