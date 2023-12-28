72d86192bb2fd09bb73b5247401add6f00f5a876
JJY10091 <nuguryzzang@gmail.com>
2023-12-28 15:29:20 +0900

    1228  병합전

--- md_exam/src/main/resources/static/js/loginCheck.js
+++ md_exam/src/main/resources/static/js/loginCheck.js
@@ -1,26 +1,24 @@
- let config = document.querySelector("input[name=configCode]").value;
- function loginCheck(action,boardId,configCode,e){
-        e.preventDefault();
+function loginCheck(action,boardId,configCode,e){
+    e.preventDefault();
 
-//        if(configCode == ""){
-//            configCode = document.querySelector("input[name=configCode]").value;
-//        }
-        alert(configCode);
+    if(configCode == ""){
+        configCode = document.querySelector("input[name=configCode]").value;
+    }
 
-//        $.ajax({
-//            type:'GET',
-//            url :'/checkLogin',
-//            success: function(result){
-//                if(!result.isLogin) {
-//                    alert("로그인이 필요합니다");
-//                }else{
-//                    if (action == 'view') {
-//                        location.href = "/board/boardView?configCode=" + configCode + "&id=" + boardId;
-//                    } else if (action == 'write') {
-//                        location.href = "/board/boardWrite?configCode=" + configCode;
-//                    }
-//                }
-//            }
-//        });
-        return false;
+    $.ajax({
+        type:'GET',
+        url :'/checkLogin',
+        success: function(result){
+            if(!result.isLogin) {
+                alert("로그인이 필요합니다");
+            }else{
+                if (action == 'view') {
+                    location.href = "/board/boardView?configCode=" + configCode + "&id=" + boardId;
+                } else if (action == 'write') {
+                    location.href = "/board/boardWrite?configCode=" + configCode;
+                }
+            }
+        }
+    });
+    return false;
 }
