59c5c867fcfbf70eb52a146b5df8a8f6f4e0f01c
JJY10091 <nuguryzzang@gmail.com>
2023-12-29 16:26:52 +0900

    장 부분 테스트 완

--- src/main/java/com/example/project/controller/MainController.java
+++ src/main/java/com/example/project/controller/MainController.java
@@ -1,14 +1,27 @@
 package com.example.project.controller;
 
+import com.example.project.dto.BoardDto;
+import com.example.project.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController {
+    @Autowired
+    BoardService boardService;
     @GetMapping("/index")
-    public String getIndex(){
+    public String getIndex(Model model){
+        List<BoardDto> noticeList = boardService.getBoard("Notice",1,"","");
+        List<BoardDto> qnaList = boardService.getBoard("QnA",1,"","");
+
+        model.addAttribute("Notice", noticeList.subList(0, Math.min(noticeList.size(), 5)));
+        model.addAttribute("noticeCode","Notice");
+        model.addAttribute("QnA",qnaList.subList(0, Math.min(qnaList.size(), 5)));
+        model.addAttribute("qnaCode","QnA");
         return "index";
     }
-
-
 }
